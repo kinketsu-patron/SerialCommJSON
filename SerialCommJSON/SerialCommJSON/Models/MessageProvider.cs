2606ea13fff656f7b4f1b8bb601d622a17b0d6f4
yoshiosan <i.am.a.passing.kamen.rider@gmail.com>
2024-11-01 00:00:26 +0900

    SerialCommJSONプロジェクト作成

---
+++ SerialCommJSON/SerialCommJSON/Models/MessageProvider.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace SerialCommJSON.Models
+{
+    public class MessageProvider : BindableBase
+    {
+        private string m_RecvMessage = "";
+        public string RecvMessage
+        {
+            get { return m_RecvMessage; }
+            set { SetProperty( ref m_RecvMessage, value ); }
+        }
+
+        public void SetMessage( string p_Message )
+        {
+            RecvMessage = string.Concat( m_RecvMessage, p_Message );
+        }
+    }
+}
---
+++ SerialCommJSON/SerialCommJSON/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using SerialCommJSON.Models;
+using System.Collections.ObjectModel;
+
+namespace SerialCommJSON.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private MessageProvider m_MessageProvider = null;
+        private SerialPortWrapper m_SerialPortWrapper = null;
+
+        public DelegateCommand ConnectCommand { get; }
+        public DelegateCommand DisconnectCommand { get; }
+
+        public ObservableCollection<string> PortList
+        {
+            get { return m_SerialPortWrapper.PortList; }
+            set { m_SerialPortWrapper.PortList = value; }
+        }
+
+        public string SelectedPort
+        {
+            get { return m_SerialPortWrapper.SelectedPort; }
+            set { m_SerialPortWrapper.SelectedPort = value; }
+        }
+
+        public string RecvMessage
+        {
+            get { return m_MessageProvider.RecvMessage; }
+            set { m_MessageProvider.RecvMessage = value; }
+        }
+
+        public MainWindowViewModel( )
+        {
+            m_MessageProvider = new MessageProvider( );
+            m_SerialPortWrapper = new SerialPortWrapper( m_MessageProvider );
+
+            m_MessageProvider.PropertyChanged += ( sender, e ) => RaisePropertyChanged( e.PropertyName );
+            m_SerialPortWrapper.PropertyChanged += ( sender, e ) => RaisePropertyChanged( e.PropertyName );
+
+            ConnectCommand = new DelegateCommand( Connect_Click );
+            DisconnectCommand = new DelegateCommand( Disconnect_Click );
+        }
+
+        private void Connect_Click( )
+        {
+            m_SerialPortWrapper.Connect( );
+        }
+
+        private void Disconnect_Click( )
+        {
+            m_SerialPortWrapper.Disconnect( );
+        }
+    }
+}
